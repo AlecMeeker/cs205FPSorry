e3952167ffef3f9c22479e6176c3fe413b43b352
Joshenberg <jfrothen@uvm.edu>
2018-04-15 21:47:05 -0400

    fixin board

--- Main/src/Block.java
+++ Main/src/Block.java
@@ -2,11 +2,22 @@ public class Block {
     Block id;
     Color color;
     Block previousBlock, nextBlock;
+    Block nextSafetyBlock;
     Pawn pawnHere;
 
     public Block(Color inColor, int id) {
         this.color = inColor;
         this.id = id;
+    }
+
+    //returns the first safety block if it's the right color and location
+    public Block getNextBlock(Color pawnColor) {
+        if (color == pawnColor && nextSafetyBlock != null) {
+            return nextSafetyBlock;
+        }
+        else {
+            return getNextBlock();
+        }
     }
 
     public Block getNextBlock() {
