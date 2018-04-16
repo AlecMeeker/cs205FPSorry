e250d7a1655b4211bdf3b33f274a9679251b1009
AMeeker <ameeker@uvm.edu>
2018-04-16 17:33:56 -0400

    develop block and board block, initialize start and home

--- Main/src/Block.java
+++ Main/src/Block.java
@@ -1,8 +1,33 @@
 package Main.src;
 
 public class Block {
-    SafetyColor SC;
-    Pawn onBlock;
+    Color SC;
+    Pawn[] onBlock;
+    int pawns;
 
+    Block(Color inC, Pawn[] inp){
+        SC=inC;
+        onBlock=inp;
+        pawns=inp.length;
+    }
+
+    Block(){
+        SC=Color.NULL;
+        onBlock=null;
+    }
+
+    public void setColor(Color SC) {
+        this.SC = SC;
+    }
+
+    public void addPawn(Pawn adp){
+        onBlock[pawns]=adp;
+        pawns++;
+    }
+
+    public void removePawn(Pawn nulpawn){
+        pawns--;
+        onBlock[pawns]=nulpawn;
+    }
 
 }
