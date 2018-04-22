45941ff1644616bfa3a356f11a6dbdb952e6f1ad
AMeeker <ameeker@uvm.edu>
2018-04-22 15:09:37 -0400

    merging?

--- Main/src/Board.java
+++ Main/src/Board.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 
 public class Board {
     Pawn nullPawn;// for all instances there is no pawn
-    // blocks 0-3 homes,4-63 regular boardblocks, 64-83 safety, 84-87 home blocks
+    // blocks 0-3 starts,4-63 regular boardblocks, 64-83 safety, 84-87 home blocks
     Block[] gameboard=new Block[88];
     //pawns, 0-3 you 4-7 ai1 8-11 ai2 12-15 ai3
     Pawn[] pieces=new Pawn[16];
